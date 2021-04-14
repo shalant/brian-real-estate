25bc10fd2622936b17f9eed5cfbff82fdb4c1cf2
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-04-13 22:28:04 -0500

    finished nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Hero from './components/Hero';
 import Navbar from './components/Navbar'
+import GlobalStyle from './globalStyles';
 
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Navbar />
+      <Hero />
     </>
   );
 }
